fec54613834504d2b322aa1f4110e2a644c5ec3f
Enordentoft <Enfeilberg@gmail.com>
2015-12-07 12:45:22 +0100

    Something something check

--- Borneland/src/java/ScoreObject.java
+++ Borneland/src/java/ScoreObject.java
@@ -1,3 +1,6 @@
+
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,18 +17,18 @@ public class ScoreObject {
     private String participantID;
     private String raceID;
     private String roundNumber;
-    private String result;
     private String fName;
     private String lName;
     private String laneID;
     private String numberOfRounds;
+    private List<String> resultList;
 
     public ScoreObject(String scoreID, String participantID, String raceID, String roundNumber, String result) {
         this.scoreID = scoreID;
         this.participantID = participantID;
         this.raceID = raceID;
         this.roundNumber = roundNumber;
-        this.result = result;
+        resultList.add(result);
     }
 
     public String getfName() {
@@ -76,14 +79,18 @@ public class ScoreObject {
         return roundNumber;
     }
 
-    public String getResult() {
-        return result;
+    public List<String> getResultList() {
+        return resultList;
+    }
+    
+    public void addResultToList(String result) {
+        resultList.add(result);
     }
 
     @Override
     public String toString() {
         return " scoreID: " + scoreID + " participantID: " + participantID + " raceID: " + raceID + " roundNumber: " + roundNumber
-                + " result: " + result + " fName: " + fName + " lName: " + lName + " laneID: " + laneID + " numberOfRounds: " + numberOfRounds;
+                + " result: " + resultList + " fName: " + fName + " lName: " + lName + " laneID: " + laneID + " numberOfRounds: " + numberOfRounds;
 
     }
 
